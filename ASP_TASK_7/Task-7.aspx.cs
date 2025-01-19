35fac3b8ee46cfc9a9e031af06d98989c155334c
Raksha551 <raksharm@acemicromatic.com>
2025-01-19 17:33:36 +0530

    Update Task-7.aspx.cs

--- ASP_TASK_7/Task-7.aspx.cs
+++ ASP_TASK_7/Task-7.aspx.cs
@@ -1,4 +1,4 @@
-﻿using Evaluation2_Screens;
+using Evaluation2_Screens;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -12,33 +12,32 @@ using Newtonsoft.Json;
 namespace ASP_TASK_7
 {
     public partial class Task_7 : System.Web.UI.Page
-    { 
-        DataBaseHelper db=new DataBaseHelper();
-        DataTable dt_frontend=new DataTable();
+    {
+        DataBaseHelper db = new DataBaseHelper();
+        DataTable dt_frontend = new DataTable();
         DataTable dt_calendar = new DataTable();
         DataTable dt_approve = new DataTable();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (IsPostBack)
             {
-               
+
             }
         }
-
-     public void ViewButton_Click(object sender, EventArgs e)
+        public void ViewButton_Click(object sender, EventArgs e)
         {
             try
             {
-              
+                DataBaseHelper.MonthYear.selectedYear = Convert.ToInt32(year.Text);
+                DataBaseHelper.MonthYear.selectedMonth = Convert.ToInt32(month.Text);
+
                 dt_frontend = db.frontEndTable();
                 dt_calendar = db.calendartable();
                 dt_approve = db.approvalTable();
 
-                int selectedYear = Convert.ToInt32(year.Text); 
-                int selectedMonth = Convert.ToInt32(month.Text);
-                int daysInMonth = DateTime.DaysInMonth(selectedYear, selectedMonth);  
-                bool isLeapYear = DateTime.IsLeapYear(selectedYear);
-
+              
+                int daysInMonth = DateTime.DaysInMonth(DataBaseHelper.MonthYear.selectedYear, DataBaseHelper.MonthYear.selectedMonth);
+     
                 List<DataBaseHelper.columnNames_DB> list = new List<DataBaseHelper.columnNames_DB>();
 
                 if (dt_frontend != null && dt_frontend.Rows.Count > 0)
@@ -47,14 +46,13 @@ namespace ASP_TASK_7
                     {
                         if (i == 0)
                         {
-                           
                             DataBaseHelper.columnNames_DB Row1 = new DataBaseHelper.columnNames_DB
                             {
                                 CheckpointID = "Sl No.",
                                 CheckpointItem = "Checkpoint Desc.",
                                 Requirement = "Requirement",
                                 Method = "Method", // Static name for Method
-                                Instrument = "Instruments", 
+                                Instrument = "Instruments",
                                 ActionPlan = "Action Plan",
                                 RowSpan = 2,
                                 tdVisible = true,
@@ -73,7 +71,7 @@ namespace ASP_TASK_7
                             DataBaseHelper.columnNames_DB Row2 = new DataBaseHelper.columnNames_DB
                             {
                                 tdVisible = false,
-                                IsHeaderRow = true 
+                                IsHeaderRow = true
                             };
 
                             List<DataBaseHelper.childClass> dynValues_Row2 = new List<DataBaseHelper.childClass>();
@@ -81,7 +79,7 @@ namespace ASP_TASK_7
                             {
                                 dynValues_Row2.Add(new DataBaseHelper.childClass
                                 {
-                                    dynamic_dates = day.ToString() 
+                                    dynamic_dates = day.ToString()
                                 });
                             }
                             Row2.innerlistviewdata = dynValues_Row2;
@@ -107,34 +105,29 @@ namespace ASP_TASK_7
                                 string columnName = dt_frontend.Columns[j].ColumnName;
                                 string dateValue = Convert.ToDateTime(columnName).ToString("dd-MM-yyyy");
 
-                                string calendarValue = "";  
+                                string calendarValue = "";
                                 bool isDateMatched = false;
 
-                                
+
                                 foreach (DataRow row in dt_calendar.Rows)
                                 {
                                     string calendarDate = Convert.ToDateTime(row["Date"]).ToString("dd-MM-yyyy");
                                     if (calendarDate == dateValue)
                                     {
-                                        calendarValue = row["Reason"].ToString();  
+                                        calendarValue = row["Reason"].ToString();
                                         isDateMatched = true;
                                         break;
                                     }
                                 }
                                 if (isDateMatched)
                                 {
-                                   
-                                        dynValues.Add(new DataBaseHelper.childClass
-                                        {
-                                            dynamic_dates = calendarValue,  // Set "SUNDAY" or "FESTIVAL"
-                                           // rowspan = 10,
-                                            tdVisible = true
-                                        });
-                                    
-                                  
-                                      
-                                    
-                                }
+
+                                    dynValues.Add(new DataBaseHelper.childClass
+                                    {
+                                        dynamic_dates = calendarValue, 
+                                        tdVisible = true
+                                    });
+                            }
                                 else
                                 {
                                     dynValues.Add(new DataBaseHelper.childClass
@@ -152,24 +145,25 @@ namespace ASP_TASK_7
                     DataBaseHelper.columnNames_DB operatorRow = new DataBaseHelper.columnNames_DB
                     {
                         CheckpointID = "Checked by sign",
-                        CheckpointItem = "", 
+                        CheckpointItem = "",
                         Requirement = "",
                         Method = "",
                         Instrument = "",
                         ActionPlan = "",
                         IsHeaderRow = false,
                         tdVisible = true,
-                        tdColSpan = 6 
+                        tdColSpan = 6
                     };
 
                     List<DataBaseHelper.childClass> operatorIdValues = new List<DataBaseHelper.childClass>();
 
                     for (int j = 6; j < dt_frontend.Columns.Count; j++)
                     {
-                        string columnName = dt_frontend.Columns[j].ColumnName; 
+                        string columnName = dt_frontend.Columns[j].ColumnName;
+                        string value=null;
                         string operatorID = null;
 
-                       
+
                         DataRow calendarRow = dt_calendar.AsEnumerable()
                             .FirstOrDefault(row => Convert.ToDateTime(row["Date"]).ToString("dd-MM-yyyy") == columnName);
 
@@ -180,32 +174,31 @@ namespace ASP_TASK_7
                         }
                         else
                         {
-                            
                             DataRow approvalRow = dt_approve.AsEnumerable()
                                 .FirstOrDefault(row => Convert.ToDateTime(row["Date"]).ToString("dd-MM-yyyy") == columnName);
 
                             if (approvalRow != null)
                             {
-                                operatorID = approvalRow["OperatorID"].ToString();
+                                value = approvalRow["OperatorID"].ToString();
+                                operatorID = $"<span style='color:black;'>{value}</span>";
+                               
                             }
                         }
 
-                       
+
                         operatorIdValues.Add(new DataBaseHelper.childClass
                         {
-                            dynamic_dates = operatorID ?? " " 
+                            dynamic_dates = operatorID ?? " "
                         });
                     }
 
                     operatorRow.innerlistviewdata = operatorIdValues;
                     list.Add(operatorRow);
 
-
-                  
                     DataBaseHelper.columnNames_DB buttonRow = new DataBaseHelper.columnNames_DB
                     {
                         CheckpointID = "Verified by sign",
-                        CheckpointItem = "", 
+                        CheckpointItem = "",
                         Requirement = "",
                         Method = "",
                         Instrument = "",
@@ -214,8 +207,6 @@ namespace ASP_TASK_7
                         tdVisible = true,
                         tdColSpan = 6
                     };
-
-                  
 
                     List<DataBaseHelper.childClass> buttonValues = new List<DataBaseHelper.childClass>();
 
@@ -229,12 +220,12 @@ namespace ASP_TASK_7
                             .FirstOrDefault(row => Convert.ToDateTime(row["Date"]).ToString("dd-MM-yyyy") == columnName);
 
                         if (calendarRow != null)
-                        {                           
+                        {
                             displayValue = calendarRow["Reason"].ToString();
                         }
                         else
                         {
-                           
+
                             DataRow approvalRow = dt_approve.AsEnumerable()
                                 .FirstOrDefault(row => Convert.ToDateTime(row["Date"]).ToString("dd-MM-yyyy") == columnName);
 
@@ -244,16 +235,41 @@ namespace ASP_TASK_7
                                 displayValue = $"<button id='{columnName}' style='width:100%; height:45px;color:white;background-color:deepskyblue;' onclick='handleButtonClick(this)'>Approved</button>";
                             }
                         }
-               
+
                         buttonValues.Add(new DataBaseHelper.childClass
                         {
-                            dynamic_dates = displayValue ?? "" 
+                            dynamic_dates = displayValue ?? ""
                         });
                     }
 
                     buttonRow.innerlistviewdata = buttonValues;
                     list.Add(buttonRow);
 
+                    for (int i = 2; i < list.Count; i++) // Index starts from 0, so 2 is the 3rd row
+                    {
+                        var row = list[i];
+                        var innerList = row.innerlistviewdata;
+
+                        int totalRows = list.Count;
+                        int middleRowIndex = totalRows / 2;
+
+                        foreach (var data in innerList)
+                        {
+                            if (data.dynamic_dates != "OK")
+                            {
+                                if (i == middleRowIndex)
+                                {
+                                    // Middle row with specific styling
+                                    data.dynamic_dates = $"<span style='color:black; border:none;'>{data.dynamic_dates}</span>";
+                                }
+                                else
+                                {
+                                    // Other rows with hidden styling
+                                    data.dynamic_dates = $"<span style='color:white; border:none;'>{data.dynamic_dates}</span>";
+                                }
+                            }
+                        }
+                    }
 
                     // Bind to ListView
                     listview1.DataSource = list;
